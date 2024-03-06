508f9ada5d390e43b411e5c85e5e8690952c4a81
Nicole Saß <sassnicole6@gmail.com>
2024-03-06 17:20:39 +0100

    first commit

---
+++ user-login/src/App.tsx
@@ -0,0 +1,19 @@
+import Heading from "./components/Heading"
+import { Section } from "./components/Section"
+
+function App() {
+
+
+  return (
+    <>
+    <Heading title="Welcome" />
+    <Section>Please Login To Continue</Section>
+    </>
+  )
+
+  
+  
+     
+}
+
+export default App
